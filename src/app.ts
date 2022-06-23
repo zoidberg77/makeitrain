3cf58ee3e76956bb68b6779b6bc971ccf6675ab2
Kevin Sidak <kevin.sidak@univie.ac.at>
2022-06-23 17:56:25 -0400

    finished this small project

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import "./style.css"
+
+
+
+const inputBox = document.getElementById("monthly") as HTMLInputElement;
+inputBox!.addEventListener('keydown', function (e) {
+    if (e.key === "Enter") {
+        let amount = 0;
+        const monthly = document.getElementById("monthly") as HTMLInputElement;
+        const monthlyAmount = parseInt(monthly.value);
+        document.getElementById("question")?.remove();
+        const amountField = document.getElementById("amount");
+        amountField!.innerText = `${amount}`;
+        const perSecound = monthlyAmount/(30*24*60*60);
+        window.setInterval(function(){
+            amount += perSecound;
+            const amountField = document.getElementById("amount");
+            amountField!.innerText = `${amount.toFixed(4)} $`;
+        }, 1000);
+    }
+}, false);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: { 
+    app: {
+      import: './src/app.ts',
+    },
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  watch: true,
+};
